0055ce14bc38bf39b9be882823b98d2ab11f2fe7
JameDevOfficial <136159367+JameDevOfficial@users.noreply.github.com>
2024-11-08 20:50:39 +0100

    Delete command.ps1

--- command.ps1
+++
@@ -1,2 +0,0 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-Expression "& { $(Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/JameDevOfficial/SpotifyInstaller/refs/heads/main/install.ps1') } -UninstallSpotifyStoreEdition -UpdateSpotify"
